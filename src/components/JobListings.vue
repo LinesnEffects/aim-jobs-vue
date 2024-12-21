fc8626037121758b571abd151017a244e7c7ca39
LinesnEffects <bgf2311@outlook.com>
2024-12-20 19:03:07 -0500

    Fetched data for job listings, refactored job listings to reactive, fetched single job and display data, created back button component and add job page

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -3,6 +3,7 @@ import { RouterLink } from "vue-router";
 import JobListing from "./JobListing.vue";
 import { reactive, defineProps, onMounted } from "vue";
 import axios from "axios";
+import PulseLoader from "vue-spinner/src/PulseLoader.vue";
 
 defineProps({
   limit: Number,
@@ -19,7 +20,7 @@ const state = reactive({
 
 onMounted(async () => {
   try {
-    const response = await axios.get("http://localhost:5000/jobs");
+    const response = await axios.get("/api/jobs");
     state.jobs = response.data;
   } catch (error) {
     console.error("Error fetching jobs", error);
@@ -35,9 +36,15 @@ onMounted(async () => {
       <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
         Browse Jobs
       </h2>
-      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+      <!-- Show loading spinner while loading is true -->
+      <div v-if="state.isLoading" class="text-center text-gray-500 py-6">
+        <PulseLoader />
+      </div>
+
+      <!-- Show job listing when loading is done -->
+      <div v-else class="grid grid-cols-1 md:grid-cols-3 gap-6">
         <JobListing
-          v-for="job in jobs.slice(0, limit || jobs.length)"
+          v-for="job in state.jobs.slice(0, limit || state.jobs.length)"
           :key="job.id"
           :job="job"
         />
